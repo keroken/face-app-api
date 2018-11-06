67cc20ecb6d939f42f16dd6500176313b430d9ab
Kenichi Shinagawa <kenichi.shinagawa@gmail.com>
2018-11-07 07:39:47 +0900

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controller/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-horizontal-42092',
       user : 'keroken',
       password : '',
       database : 'face-app'
