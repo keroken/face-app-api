b9485c20c4671529df2c3bf847b3c1f9009e5985
keroken <kenichi.shinagawa@gmail.com>
2022-05-05 22:41:42 +0900

    servers.js - revert CORS chages

--- server.js
+++ server.js
@@ -26,35 +26,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.set({ 'Access-Control-Allow-Origin': 'https://face-app-kero.herokuapp.com/'});
-    res.send('it is working!')
-});
+app.get('/', (req, res) => { res.send('it is working!') });
 
-app.post('/signin', (req, res) => {
-    res.set({ 'Access-Control-Allow-Origin': 'https://face-app-kero.herokuapp.com/'});
-    signin.handleSignin(req, res, db, bcrypt)
-});
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 
-app.post('/register', (req, res) => {
-    res.set({ 'Access-Control-Allow-Origin': 'https://face-app-kero.herokuapp.com/'});
-    register.handleRegister(req, res, db, bcrypt)
-});
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
-app.get('/profile/:id', (req, res) => {
-    res.set({ 'Access-Control-Allow-Origin': 'https://face-app-kero.herokuapp.com/'});
-    profile.handleProfileGet(req, res,db)
-});
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res,db) });
 
-app.put('/image', (req, res) => {
-    res.set({ 'Access-Control-Allow-Origin': 'https://face-app-kero.herokuapp.com/'});
-    image.handleImage(req, res, db)
-});
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
-app.post('/imageurl', (req, res) => {
-    res.set({ 'Access-Control-Allow-Origin': 'https://face-app-kero.herokuapp.com/'});
-    image.handleApiCall(req, res)
-});
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 app.listen(process.env.PORT || 3001, () => {
     console.log(`app is running on port ${process.env.PORT}`);
