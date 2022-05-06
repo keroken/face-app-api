5ceac66a9f1fc72f8b5d83fa454865ecdbac13f5
keroken <kenichi.shinagawa@gmail.com>
2022-05-07 01:54:50 +0900

    update server

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
@@ -15,28 +14,19 @@ const db = knex({
         connectionString: process.env.DATABASE_URL,
         ssl: { rejectUnauthorized: false }
     }
-  });
-
-db.select('*').from('users').then(data => {
-    console.log(data);
 });
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => { res.send('it is working!') });
-
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
-
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
-
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res,db) });
-
-app.put('/image', (req, res) => { image.handleImage(req, res, db) });
-
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3001, () => {
     console.log(`app is running on port ${process.env.PORT}`);
