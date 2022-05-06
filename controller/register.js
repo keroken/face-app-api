e9c9c37d24c75193a68803b0b6c94d2668305a1b
keroken <kenichi.shinagawa@gmail.com>
2022-05-07 00:19:13 +0900

    use bcrypt-nodejs and update login logic

--- controller/register.js
+++ controller/register.js
@@ -3,8 +3,7 @@ const handleRegister = (req, res, db, bcrypt) => {
     if (!email || !name || !password) {
         return res.status(400).json('incorrect form submission');
     }
-    const saltRounds = 10;
-    const hash = bcrypt.hashSync(password, saltRounds);
+    const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
         trx.insert({
             hash: hash,
